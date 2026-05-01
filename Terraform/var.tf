982dc56e5e8b069c3cccd58c6762f739ec463877
Rameshwar <ramesh.040183@gmail.com>
2026-05-01 15:49:53 +0530

    new Commit

--- Terraform/var.tf
+++ Terraform/var.tf
@@ -1,44 +1,39 @@
 variable "aws_region" {
     description = "AWS_REGION"
-    type = "string"  
+    type = string  
 }
 
 variable "vpc_cidr" {
     description = "vpc_cidr"
-    type = "string"  
+    type = string  
 }
 
 variable "subnet1_cidr" {
     description = "subnet1_cidr"
-    type = "string"  
+    type = string  
 }
 
 variable "subnet2_cidr" {
     description = "subnet2_cidr"
-    type = "string"  
+    type = string  
 }
 
 variable "availability_Zone_1" {
     description = "availability_Zone_1"
-    type = "string"  
+    type = string  
 }
 
 variable "availability_Zone_2" {
     description = "availability_Zone_2"
-    type = "string"  
+    type = string  
 }
 
 variable "ami_id" {
     description = "ami_id"
-    type = "string"  
+    type = string  
 }
 
 variable "instance_type" {
     description = "instance_type"
-    type = "string"  
+    type = string 
 }
-
-
-
-
-
